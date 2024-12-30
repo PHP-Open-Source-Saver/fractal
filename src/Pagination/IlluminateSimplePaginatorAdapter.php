8630339c1bc58c4e048465cb5658ba398c3c9dc5
Max <contact@maxsnow.me>
2024-12-31 03:47:26 +1100

    fix copyright

--- src/Pagination/IlluminateSimplePaginatorAdapter.php
+++ src/Pagination/IlluminateSimplePaginatorAdapter.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the PHPOpenSourceSaver\Fractal package.
  *
- * (c) Phil Sturgeon <me@philsturgeon.uk>
+ * (c) Max Snow <contact@maxsnow.me>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
